27051aa26dee22d05f87b9f82961f76d90417138
luoqian <luoq04@mingyuanyun.com>
2022-09-11 17:10:25 +0800

    feat: 问卷第一版,一组四次

--- src/components/Questionnaire/index.tsx
+++ src/components/Questionnaire/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import "./index.scss";
+import "./styles.css";
 import usePrevious from "./hooks/usePrevious";
 
 type Props = {};
@@ -38,6 +38,9 @@ export default function Questionnaire({}: Props) {
 
   const [isOver, setOver] = useState(false);
 
+  const [loading, setLoading] = useState(false);
+  const [lock, setLock] = useState(false);
+
   /** 随机设置分组 */
   useEffect(() => {
     const group = Math.random() > 0.5 ? groupA : groupB;
@@ -47,12 +50,26 @@ export default function Questionnaire({}: Props) {
 
   const handleOver = () => {
     let result = Math.abs(LD - HU) <= 10;
+    console.log(
+      "handleOver",
+      `Math.abs(${LD} - ${HU}) <= 10`,
+      Math.abs(LD - HU),
+      result
+    );
     if (result) {
       const nextRound = group[round + 1];
       if (nextRound) {
-        setRound(round + 1);
-        setOptOne(defaultSS);
-        setOptTwo(defaultLL);
+        setLoading(true);
+        setTimeout(() => {
+          setRound(round + 1);
+          setOptOne(defaultSS);
+          setOptTwo(defaultLL);
+          // init LD and HU
+          setHU(500);
+          setLD(0);
+          firstCountRef.current = false;
+          setLoading(false);
+        }, 1000);
         result = false;
       } else {
         setOver(true);
@@ -62,17 +79,15 @@ export default function Questionnaire({}: Props) {
   };
 
   useEffect(() => {
-    console.log(mountedRef.current);
     if (!mountedRef.current) {
       mountedRef.current = true;
       return;
     }
-    console.log("useEffect");
 
     const m = Math.abs((LD - HU) / 2);
     let nextOptTwo = 0;
-    if (HU !== prevHU) {
-      //
+    if (HU !== prevHU && lock) {
+      console.log("hu changed");
       if (!firstCountRef.current) {
         firstCountRef.current = true;
         const m = Math.abs((LD - prevHU!) / 2);
@@ -81,16 +96,23 @@ export default function Questionnaire({}: Props) {
         console.log(`本次计算为${optTwo} + (${LD} - ${HU}) / 2`);
         nextOptTwo = getFloorInt(optTwo + m);
       }
+      setOptTwo(nextOptTwo);
     }
-    if (LD !== prevLD) {
+
+    if (LD !== prevLD && lock) {
+      firstCountRef.current = true;
+      console.log("ld changed");
       console.log(`本次计算为${optTwo} - (${LD} - ${HU}) / 2`);
       nextOptTwo = getFloorInt(optTwo - m);
+      setOptTwo(nextOptTwo);
     }
-    setOptTwo(nextOptTwo);
+    handleOver();
+    setLock(false);
   }, [HU, LD]);
 
   const onSelect = (option: number) => {
-    if (handleOver()) return;
+    if (lock || isOver) return;
+    setLock(true);
     if (option === 0) {
       setHU(optTwo);
     }
@@ -108,24 +130,38 @@ export default function Questionnaire({}: Props) {
   };
   return (
     <div>
-      <div>
+      <div className="content">
+        你通过银行的抽奖活动，获得了一次兑换现金奖励的机会。但是这需要你在两种类别的奖项里进行选择，两个奖项会在
+        <span>不同的时间兑现，金额也并不相同</span>
+        。所以请您在后续的选择中，做出你心中<span>理想的选择</span>
+        。谢谢您的合作，我们将<span>不会获取您的个人信息</span>
+        ，完成选择后的数据我们也将仅用于科学研究。感谢您的配合。
+      </div>
+      <div className="title">
         本次测试有{group.length}轮，当前为第{round + 1}轮
       </div>
-      <p>假设你购买了一个奖券并中奖，你可以在两种奖励当中选择一种：</p>
-      <div className="option_container">
-        {Math.random() > 0.5 ? (
-          <>
-            {OptionItem(0)}
-            {OptionItem(1)}
-          </>
-        ) : (
-          <>
-            {OptionItem(1)}
-            {OptionItem(0)}
-          </>
-        )}
-      </div>
-      {isOver && <div>完成问卷</div>}
+      <p className="question">
+        假设你购买了一个奖券并中奖，你可以在两种奖励当中选择一种：
+      </p>
+      {loading ? (
+        <div>正在加载下一轮。。。</div>
+      ) : (
+        <div className="option_container">
+          {Math.random() > 0.5 ? (
+            <>
+              {OptionItem(0)}
+              {OptionItem(1)}
+            </>
+          ) : (
+            <>
+              {OptionItem(1)}
+              {OptionItem(0)}
+            </>
+          )}
+        </div>
+      )}
+
+      {isOver && <div className="over_content">恭喜你，完成问卷!</div>}
     </div>
   );
 }
