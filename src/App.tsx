4bf43b8cef0ac8c74f2512376171156e51814050
qian.luo <qian.luo@traveloka.com>
2025-11-19 17:34:51 +0800

    feat: test change src code

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import ToggleList from "./components/ToggleList";
 import Questionnaire from "./components/Questionnaire";
 
 function App() {
-  // console.log('App render test source code change ');
+  console.log('App render test source code change ');
   // const [list, setList] = useState([1, 2]);
   // const [fold, setFold] = useState(true);
   // const ulRef = useRef<HTMLUListElement>(null);
